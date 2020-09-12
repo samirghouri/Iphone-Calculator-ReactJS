2506217eadc2e27966f2c7d5c298e96ebdd1a003
samirghouri <samira.ghouri@st.niituniversity.in>
2020-09-12 16:27:50 +0530

    Updates

---
+++ precache-manifest.1840e84e9bc1a9d7796242c3ffbb2d83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90061e00d24db58ba062db099d24680",
+    "url": "/Iphone-Calculator-ReactJS/index.html"
+  },
+  {
+    "revision": "cc1b6fa0bc9f0a176d6b",
+    "url": "/Iphone-Calculator-ReactJS/static/css/main.015843c7.chunk.css"
+  },
+  {
+    "revision": "80265475b43b295ba063",
+    "url": "/Iphone-Calculator-ReactJS/static/js/2.33de511c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Iphone-Calculator-ReactJS/static/js/2.33de511c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1b6fa0bc9f0a176d6b",
+    "url": "/Iphone-Calculator-ReactJS/static/js/main.9f1ae0e8.chunk.js"
+  },
+  {
+    "revision": "bc84ad65d323b067935f",
+    "url": "/Iphone-Calculator-ReactJS/static/js/runtime-main.0cdc885e.js"
+  }
+]);
